68bba40cd387ca9c0ad9718d23ccd82ee84615f1
olisadunkwu <nathanielolisadunkwu03@gmail.com>
2023-06-16 03:11:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program thats prints a line with puts function
+* Return: 0 (success)
+*/
+int main(void)
+(
+puts("\"Programming is like building a multilingual puzzl e");
+return (0);
+)
